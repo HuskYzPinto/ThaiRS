d23257d33c892177513bab5e05275741c61de84a
Huskiez <huskiez@pop-os.localdomain>
2021-08-23 02:39:13 +1000

    add gh pages to dep

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,9 +3,19 @@ import { PageProps } from "gatsby";
 import { Box } from "@material-ui/core";
 
 import Layout from "../components/layout";
+import { Helmet } from "react-helmet";
 
 const Error: React.FC<PageProps> = (props: PageProps) => {
-  return <Layout> Hello, you exploded something there! </Layout>;
+  return (
+    <Layout>
+      {" "}
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title> Error 404: Page Not Found </title>
+      </Helmet>{" "}
+      Hello, you exploded something there!{" "}
+    </Layout>
+  );
 };
 
 export default Error;
