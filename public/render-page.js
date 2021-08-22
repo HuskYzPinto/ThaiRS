8e3a8fd3e75c74cc3658a30cadbbc1de83e39ce8
Huskiez <huskiez@pop-os.localdomain>
2021-08-23 01:34:32 +1000

    move project to root

--- public/render-page.js
+++ public/render-page.js
@@ -7117,24 +7117,24 @@ function v(){return(v=Object.assign||function(e){for(var t=1;t<arguments.length;
 /***/ }),
 
 /***/ "react-dom/server":
-/*!*****************************************************************************************!*\
-  !*** external "/home/huskiez/projects/ThaiRS/thai-rs/node_modules/react-dom/server.js" ***!
-  \*****************************************************************************************/
+/*!*********************************************************************************!*\
+  !*** external "/home/huskiez/projects/ThaiRS/node_modules/react-dom/server.js" ***!
+  \*********************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/home/huskiez/projects/ThaiRS/thai-rs/node_modules/react-dom/server.js");
+module.exports = require("/home/huskiez/projects/ThaiRS/node_modules/react-dom/server.js");
 
 /***/ }),
 
 /***/ "react":
-/*!************************************************************************************!*\
-  !*** external "/home/huskiez/projects/ThaiRS/thai-rs/node_modules/react/index.js" ***!
-  \************************************************************************************/
+/*!****************************************************************************!*\
+  !*** external "/home/huskiez/projects/ThaiRS/node_modules/react/index.js" ***!
+  \****************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/home/huskiez/projects/ThaiRS/thai-rs/node_modules/react/index.js");
+module.exports = require("/home/huskiez/projects/ThaiRS/node_modules/react/index.js");
 
 /***/ }),
 
